be91e5463f358fe59124dfc9d783e0e979703ce3
Rafael Antunes <dev@rafaelantun.es>
2021-05-12 16:28:10 -0300

    flow changes

---
+++ airports/lib/airports.ex
@@ -0,0 +1,27 @@
+defmodule Airports do
+
+  alias NimbleCSV.RFC4180, as: CSV
+
+  @spec airports_csv :: binary
+  def airports_csv() do
+    Application.app_dir(:airports, "/priv/airports.csv")
+  end
+
+  @spec open_airports :: list
+  def open_airports() do
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+        [row] = CSV.parse_string(row, skip_headers: false)
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Enum.to_list()
+  end
+end
