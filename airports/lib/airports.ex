fe04bf2054883454c8b35c8d415201c5c65a3aa7
Rafael Antunes <dev@rafaelantun.es>
2021-05-13 13:16:02 -0300

    airport project done, added flow trigger and window

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -29,4 +29,44 @@ defmodule Airports do
     |> Enum.to_list()
     |> List.flatten()
   end
+
+  def open_trigger() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
+
+    airports_csv()
+    |> File.stream!()
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0,0,0,1]))
+      event
+    end)
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+        [row] = CSV.parse_string(row, skip_headers: false)
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(window: window, key: {:key, :country})
+    |> Flow.group_by(& &1.country)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done ->
+          {events, acc}
+        {:every, 1000} ->
+          {[], acc}
+      end
+    end)
+    |> Enum.sort(fn {_, a}, {_, b} -> a > b end)
+    |> Enum.take(10)
+    |> List.flatten()
+  end
 end
